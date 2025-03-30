5cd5fea3092c0dce7ec9b7bb2610155d4bed4919
arvindkumar20 <arvindkumar8960905167@gmail.com>
2025-03-30 22:34:23 +0530

    remove .env

--- index.js
+++ index.js
@@ -27,12 +27,3 @@ app.listen(5000, () => {
     console.log("Server running on port 5000");
 });
 
-
-
-// Password= "OOMeZ8UJdLHmVlh7"
-// MongoDB="mongodb+srv://anjaliverma95062:OOMeZ8UJdLHmVlh7@aiimagegenrater.zwnfz.mongodb.net/?retryWrites=true&w=majority&appName=AiImagegenrater"
-// UNSPLASH_ACCESS_KEY="3BJ7zYGzwEZ5olJl8KJPRJnHJ3_wISWPsZMkM2Ib9tE"
-// CLOUDINARY_CLOUD_NAME="ddnsg2woa"   
-// CLOUDINARY_API_KEY="467511194623355"
-// CLOUDINARY_API_SECRET="nU1-emBD7hlFm3NNydfZMXH1O80"
-// IMAGEPIG_API_KEY="403797d3-f972-4396-82c1-362f42c96185"
